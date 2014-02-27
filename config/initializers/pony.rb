f41b49af25283013643133f7837335a2587e4939
nzekic <naida.696@gmail.com>
2014-02-27 21:32:45 +0100

    Minor changes

--- config/initializers/pony.rb
+++ config/initializers/pony.rb
@@ -1,13 +1,13 @@
 #require 'pony'
 Pony.options = {
-  :to => 'naida.696@gmail.com',
+  :to => 'fitness.tutorial.ror@gmail.com',
   :via => :smtp,
   :via_options => {
     :address              => 'smtp.gmail.com',
     :port                 => '587',
     :enable_starttls_auto => true,
-    :user_name            => 'naida.696',
-    :password             => 'h6bjkhhq',
+    :user_name            => 'fitness.tutorial.ror',
+    :password             => 'fitnesstutorialror',
     :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
     :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
   }
